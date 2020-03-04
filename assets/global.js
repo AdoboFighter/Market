2043f81bc86948b38ed3a7bb60cd24dc53716971
macrowie <mcalbertocapistrano@gmail.com>
2020-03-04 11:51:39 +0800

    awdawdawd

--- assets/global.js
+++ assets/global.js
@@ -1,11 +1,8 @@
-var global  = {
-  settings : {
-   url : 'http://192.168.3.155/'
-   // url : 'http://68dbbedd.ngrok.io'
-    //url : 'https://4502482d.ngrok.io'
-
-
-
-  },
-
-}
+var global = {
+	settings: {
+		url: "http://localhost:3000/"
+		//  url : 'http://192.168.3.155/'
+		// url : 'http://68dbbedd.ngrok.io'
+		//url : 'https://4502482d.ngrok.io'
+	}
+};
